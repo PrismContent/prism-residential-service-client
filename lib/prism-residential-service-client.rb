6b52b614b107890dc09aaee977bd65369dac7aa6
Andy Vanasse <andyvanasse@gmail.com>
2012-04-20 13:57:45 -0400

    Make inclusion of HashWithIndifferentAccess optional (2.3.x stable).

--- lib/prism-residential-service-client.rb
+++ lib/prism-residential-service-client.rb
@@ -6,7 +6,11 @@ Bundler.require
 # Support files first
 require 'active_support/core_ext/hash'
 require 'active_support/json'
-require 'active_support/hash_with_indifferent_access'
+
+begin
+  require 'active_support/hash_with_indifferent_access'
+rescue
+end
 
 filepath = File.join( File.dirname(__FILE__), 'support', '*.rb' )
 Dir.glob(filepath){|fname| require fname }
